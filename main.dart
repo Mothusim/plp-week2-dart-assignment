23fe336cd1b4cb8dc9b43d3ab9eb6bda70e5b007
Mothusi Mathuloe <111005114+Mothusim@users.noreply.github.com>
2024-03-07 08:58:48 +0200

    dart week2 assignment done

---
+++ main.dart
@@ -0,0 +1,23 @@
+// Dart program demonstrating different data types
+
+void main() {
+  // Integer data type: Used to store whole numbers
+  int number = 42;
+  print('Integer: $number');
+
+  // Double data type: Used to store decimal numbers
+  double doubleNumber = 3.14;
+  print('Double: $doubleNumber');
+
+  // String data type: Used to store textual data
+  String message = 'Hello, Dart!';
+  print('String: $message');
+
+  // List data type: Used to store a collection of elements
+  List<int> numbersList = [1, 2, 3, 4, 5];
+  print('List: $numbersList');
+
+  // Map data type: Used to store key-value pairs
+  Map<String, int> map = {'one': 1, 'two': 2, 'three': 3};
+  print('Map: $map');
+}
